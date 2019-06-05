5b8aea4302ed45545312734ba9ac1b9373bc31b8
Leander Rodrigues <me@leander.xyz>
2019-06-05 15:56:18 -0400

    🐛 Setup parcel bunder on vue

---
+++ Vallery/src/components/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>{{name}}</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: function() {
+    return {
+      name: "Hello d!"
+    };
+  }
+});
+</script>
