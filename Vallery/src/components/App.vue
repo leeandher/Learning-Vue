6505b5a5c6b03a773a3a3e2b21c844fb1cdd8e2d
Leander Rodrigues <me@leander.xyz>
2019-06-12 22:20:20 -0400

    🏷️ Implement TS w/ Vue

--- Vallery/src/components/App.vue
+++ Vallery/src/components/App.vue
@@ -1,6 +1,8 @@
 
 <template>
-  <div>{{name}}</div>
+  <div class="app-wrap">
+    <h1 class="page-title">{{pageTitle}}</h1>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,8 +11,23 @@ import Vue from "vue";
 export default Vue.extend({
   data: function() {
     return {
-      name: "Hello d!"
+      pageTitle: "IT Works!"
     };
   }
 });
 </script>
+
+<style lang="scss">
+html {
+  font-size: 10px;
+  font-family: "Franklin Gothic Medium", "Futura", "Arial Condensed",
+    "Helvetica", sans-serif;
+}
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+
