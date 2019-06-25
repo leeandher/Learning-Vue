e9ad041aa6cc97c07b3569e38a71567d8c4145e4
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-06-25 16:26:37 -0400

    ✨ Implement new footers:

--- Vallery/src/app.ts
+++ Vallery/src/app.ts
@@ -1,6 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faLinkedin,
+  faGithub,
+  faStackOverflow,
+  faHackerrank
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faLinkedin, faGithub, faStackOverflow, faHackerrank, faAt);
+Vue.component("fa-icon", FontAwesomeIcon);
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- Vallery/src/components/Footer.vue
+++ Vallery/src/components/Footer.vue
@@ -1,19 +1,99 @@
 <template>
-  <div class="footer-wrapper">Footer</div>
+  <div class="footer-wrapper" :style="style">
+    <div class="link-wrapper">
+      <a href="http://www.leander.xyz/contact">
+        <fa-icon icon="at"/>
+      </a>
+      <a href="http://github.com/leeandher">
+        <fa-icon :icon="['fab', 'github']"/>
+      </a>
+      <a href="https://www.linkedin.com/in/leander-rodrigues/">
+        <fa-icon :icon="['fab', 'linkedin']"/>
+      </a>
+      <a href="https://stackoverflow.com/users/story/10996907">
+        <fa-icon :icon="['fab', 'stack-overflow']"/>
+      </a>
+      <a href="https://www.hackerrank.com/LeeAndHer">
+        <fa-icon :icon="['fab', 'hackerrank']"/>
+      </a>
+    </div>
+    <p class="tag">
+      🛠️ with ❤️ and 🎵 by
+      <a href="https://www.leander.xyz">👨‍💻Leander👨‍💻</a>
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 export default Vue.extend({
-  name: "Footer"
+  name: "Footer",
+  props: ["config"],
+  computed: {
+    style() {
+      const {
+        mainText,
+        altText,
+        darkAccent,
+        accent,
+        lightAccent,
+        altAccent
+      } = this.config;
+      return `
+        background-color: ${accent};
+        color: ${mainText};
+      `;
+    }
+  }
 });
 </script>
 
 
-<style lang="scss">
+<style lang="scss" scoped>
 .footer-wrapper {
-  background: aqua;
-  font-size: 1.75rem;
+  font-size: 1.55rem;
+  min-height: 200px;
+}
+.tag {
+  margin: 0;
+  text-align: center;
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+}
+.link-wrapper {
+  text-align: center;
+  padding: 2rem;
+  a {
+    display: inline-block;
+    position: relative;
+    margin: 1rem;
+    color: inherit;
+    width: 75px;
+    height: 75px;
+    &:before {
+      content: "";
+      position: absolute;
+      background: white;
+      transition: all 0.1s cubic-bezier(0.165, 0.84, 0.44, 1);
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      max-height: 0%;
+    }
+    &:hover:before {
+      max-height: 100%;
+    }
+  }
+  svg {
+    font-size: 3rem;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+  }
 }
 </style>
 
